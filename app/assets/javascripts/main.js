fa887cbc240ee7f3e96bdf746a831c1b8b1477dc
JoshSutcliffe <joshclement12@gmail.com>
2016-05-17 15:36:22 +0100

    layout sorted

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,11 @@
 console.log("js connected")
 
+// initalise parralax
 $(document).ready(function(){
   $('.parallax').parallax();
 });
+
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+//$('.collapsible').collapsible();
