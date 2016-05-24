604903875825f601f1bb64e4d4c3d29537e3bdad
JoshSutcliffe <joshclement12@gmail.com>
2016-05-24 21:06:04 +0100

    working on image fade

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,5 +4,13 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 
+    var options = [
+      {selector: '#image-test', offset: 150, callback: function() {
+        Materialize.fadeInImage("#image-test");
+        console.log('now')
+      } }
+    ];
+    Materialize.scrollFire(options);
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
