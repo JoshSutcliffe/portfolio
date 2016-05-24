2b741c3595915c9bc8702838bafc38658215a230
JoshSutcliffe <joshclement12@gmail.com>
2016-05-24 14:28:04 +0100

    changed layout

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,9 +3,5 @@ console.log("js connected")
 // initalise parralax
 $(document).ready(function(){
   $('.parallax').parallax();
+  $(".button-collapse").sideNav();
 });
-
-// Initialize collapse button
-$(".button-collapse").sideNav();
-// Initialize collapsible (uncomment the line below if you use the dropdown variation)
-//$('.collapsible').collapsible();
